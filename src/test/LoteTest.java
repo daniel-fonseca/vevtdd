b71803df2ba12baed3838932f21b299a22d95926
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-12-13 13:15:45 -0300

    ajuste de lotetest por conta das alteracoes no construtor de ingresso

--- src/test/LoteTest.java
+++ src/test/LoteTest.java
@@ -3,21 +3,20 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 import model.Lote;
 import model.Ingresso;
+import model.TipoIngresso;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 class LoteTest {
 
     @Test
-    void deveCriarLoteComIngressos() {
+    void todosIngressosDoLoteDevemSerNaoVendidosAoCriar() {
         List<Ingresso> ingressos = List.of(
-                new Ingresso(1, TipoIngresso.NORMAL, false, 10.0),
-                new Ingresso(2, TipoIngresso.VIP, false, 20.0)
+                new Ingresso(1, TipoIngresso.NORMAL, 10.0),
+                new Ingresso(2, TipoIngresso.VIP, 20.0)
         );
-        Lote lote = new Lote(1, ingressos, 0.10); // 10% de desconto
-        assertEquals(1, lote.getId());
-        assertEquals(2, lote.getIngressos().size());
-        assertEquals(0.10, lote.getDesconto());
+        Lote lote = new Lote(1, ingressos, 0.10);
+        assertTrue(lote.getIngressos().stream().allMatch(ingresso -> !ingresso.isVendido()));
     }
 
 }
