1f4ca1608bf6064d8aefeb16971079f0f09b883e
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-12-13 13:52:08 -0300

    feat: adição dos métodos iniciais de Show

--- src/model/Show.java
+++ src/model/Show.java
@@ -3,5 +3,32 @@ package model;
 import java.util.List;
 
 public class Show {
+    private String data;
+    private String artista;
+    private boolean dataEspecial;
+    private List<Lote> lotes;
 
+    public Show(String data, String artista, boolean dataEspecial, List<Lote> lotes) {
+        this.data = data;
+        this.artista = artista;
+        this.dataEspecial = dataEspecial;
+        this.lotes = lotes;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+
+    public boolean isDataEspecial() {
+        return dataEspecial;
+    }
+
+    public List<Lote> getLotes() {
+        return lotes;
+    }
 }
