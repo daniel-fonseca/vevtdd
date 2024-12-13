a6d0f0e640f4d6e0f5db7ac23abed29cde57264a
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-12-13 14:05:26 -0300

    feat: novo construtor para Show

--- src/model/Show.java
+++ src/model/Show.java
@@ -5,12 +5,16 @@ import java.util.List;
 public class Show {
     private String data;
     private String artista;
+    private double cacheArtista;
+    private double despesasInfraestrutura;
     private boolean dataEspecial;
     private List<Lote> lotes;
 
-    public Show(String data, String artista, boolean dataEspecial, List<Lote> lotes) {
+    public Show(String data, String artista, double cacheArtista, double despesasInfraestrutura, boolean dataEspecial, List<Lote> lotes) {
         this.data = data;
         this.artista = artista;
+        this.cacheArtista = cacheArtista;
+        this.despesasInfraestrutura = despesasInfraestrutura;
         this.dataEspecial = dataEspecial;
         this.lotes = lotes;
     }
