d97a7cfac6dc39e6a2fb74e36a978bc95fc4eb17
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-12-13 12:51:14 -0300

    cria classe ingresso

--- src/model/Ingresso.java
+++ src/model/Ingresso.java
@@ -1,4 +1,35 @@
 package model;
 
 public class Ingresso {
+    private int id;
+    private String tipo;
+    private boolean vendido;
+    private double preco;
+
+    public Ingresso(int id, String tipo, boolean vendido, double preco) {
+        this.id = id;
+        this.tipo = tipo;
+        this.vendido = vendido;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public boolean isVendido() {
+        return vendido;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void marcarComoVendido() {
+        this.vendido = true;
+    }
 }
