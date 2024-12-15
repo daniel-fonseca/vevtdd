9d4de7240b8f7eede1598ccb7134f19b98d084d4
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-12-14 22:17:26 -0300

    fix: calculo de percentuais

--- src/model/Lote.java
+++ src/model/Lote.java
@@ -19,11 +19,11 @@ public class Lote {
     }
 
     public Ingresso criarIngresso(TipoIngresso tipo, double preco) {
+        validarPercentuais(tipo);
+
         int novoId = ingressos.size() + 1;
         Ingresso ingresso = new Ingresso(novoId, tipo, preco);
         ingressos.put(novoId, ingresso);
-
-        validarPercentuais(tipo);
 
         return ingresso;
     }
@@ -57,7 +57,7 @@ public class Lote {
     }
 
     private void validarPercentuais(TipoIngresso novoTipo) {
-        long totalIngressos = ingressos.size();
+        long totalIngressos = ingressos.size() + 1;
         long totalVIP = ingressos.values().stream().filter(i -> i.getTipo() == TipoIngresso.VIP).count();
         long totalMeiaEntrada = ingressos.values().stream().filter(i -> i.getTipo() == TipoIngresso.MEIA_ENTRADA).count();
 
@@ -67,11 +67,14 @@ public class Lote {
             totalMeiaEntrada++;
         }
 
-        if (totalVIP < 0.2 * totalIngressos || totalVIP > 0.3 * totalIngressos) {
+        double percentualVIP = (double) totalVIP / totalIngressos;
+        double percentualMeiaEntrada = (double) totalMeiaEntrada / totalIngressos;
+
+        if (percentualVIP < 0.2 || percentualVIP > 0.3) {
             throw new IllegalArgumentException("Ingressos VIP devem ser entre 20% e 30% do total.");
         }
 
-        if (totalMeiaEntrada != 0.1 * totalIngressos) {
+        if (Math.abs(percentualMeiaEntrada - 0.1) > 0.0001) {
             throw new IllegalArgumentException("Ingressos MEIA_ENTRADA devem ser 10% do total.");
         }
     }
