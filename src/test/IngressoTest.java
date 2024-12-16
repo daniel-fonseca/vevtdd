2fb45b27f3afcc9c64d3eaa2d77818b13407a1f0
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-12-16 17:34:29 -0300

    fix: removendo verificacao indevida

--- src/test/IngressoTest.java
+++ src/test/IngressoTest.java
@@ -19,22 +19,5 @@ class IngressoTest {
         ingresso.marcarComoVendido();
         assertTrue(ingresso.isVendido());
     }
-
-    @Test
-    void deveValidarPrecoIngressoVIP() {
-        Ingresso ingressoVIP = new Ingresso(1, TipoIngresso.VIP, 10.0);
-        assertEquals(20.0, ingressoVIP.getPreco(), "Ingresso VIP deve custar o dobro do ingresso NORMAL.");
-    }
-
-    @Test
-    void deveValidarPrecoIngressoMeiaEntrada() {
-        Ingresso ingressoMeiaEntrada = new Ingresso(2, TipoIngresso.MEIA_ENTRADA, 10.0);
-        assertEquals(5.0, ingressoMeiaEntrada.getPreco(), "Ingresso MEIA_ENTRADA deve custar a metade do ingresso NORMAL.");
-    }
-
-    @Test
-    void deveValidarPrecoIngressoNormal() {
-        Ingresso ingressoNormal = new Ingresso(3, TipoIngresso.NORMAL, 10.0);
-        assertEquals(10.0, ingressoNormal.getPreco(), "Ingresso NORMAL deve manter o preço original.");
-    }
+    
 }
